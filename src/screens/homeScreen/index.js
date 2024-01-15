0a0526521ed608d093e5e5ea1504f6c6f890315b
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2024-01-15 23:12:36 +0530

    refactor - added assets images in project

--- src/screens/homeScreen/index.js
+++ src/screens/homeScreen/index.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, FlatList } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { AntDesign } from '@expo/vector-icons';
@@ -6,7 +6,30 @@ import { AntDesign } from '@expo/vector-icons';
 import styles from './style';
 import saloonIcon from "../../../assets/images/saloon.png"
 
-const HomeScreen = ({navigation,route}) => {
+const grommingList = [
+  {
+    serviceType: "Hair Cut",
+    image: require("../../../assets/images/hairdresser.png")
+  },
+  {
+    serviceType: "Shave",
+    image: require("../../../assets/images/man.png")
+  },
+  {
+    serviceType: "Hair Color",
+    image: require("../../../assets/images/color.png")
+  },
+  {
+    serviceType: "Hair Straightening",
+    image: require("../../../assets/images/straightener.png")
+  },
+  {
+    serviceType: "Massage",
+    image: require("../../../assets/images/massage.png")
+  },
+]
+
+const HomeScreen = ({ navigation, route }) => {
   return (
     <SafeAreaView style={styles.mainContainer}>
       <View style={styles.headerContainer}>
@@ -14,10 +37,13 @@ const HomeScreen = ({navigation,route}) => {
           source={saloonIcon}
           style={styles.saloonImage}
         />
-      <TouchableOpacity style={styles.bookNowButtonContainer}>
-        <Text style={styles.bookNowText}>Book Now</Text>
-        <AntDesign name="arrowright" size={16} color="black" />
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.bookNowButtonContainer} onPress={() => navigation.navigate("Services")}>
+          <Text style={styles.bookNowText}>Book Now</Text>
+          <AntDesign name="arrowright" size={16} color="black" />
+        </TouchableOpacity>
+        <View>
+          <FlatList />
+        </View>
       </View>
     </SafeAreaView>
   )

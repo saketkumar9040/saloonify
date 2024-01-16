455ccc3a0b77307c07296f54e8fde81e20a2206c
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2024-01-16 22:43:07 +0530

    feat - started with services screen

--- src/screens/servicesScreen/index.js
+++ src/screens/servicesScreen/index.js
@@ -1,13 +1,60 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native'
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import styles from "./style.js"
+import styles from "./style.js";
+
+const services = [
+  {
+    type: "Hair Cut",
+    time: "20 min",
+    slot: "available"
+  },
+  {
+    type: "Shaving",
+    time: "12 min",
+    slot: "available"
+  },
+  {
+    type: "Hair coloring",
+    time: "50 min",
+    slot: "available"
+  },
+  {
+    type: "Hair Cut",
+    time: "30 min",
+    slot: "available"
+  },
+  {
+    type: "Hair Cut",
+    time: "40 min",
+    slot: "available"
+  },
+  {
+    type: "Hair Cut",
+    time: "35 min",
+    slot: "available"
+  },
+]
 
 const ServicesScreen = () => {
   return (
     <SafeAreaView>
-      <Text>ServicesScreen</Text>
+      <FlatList
+        data={services}
+        style={{ flexDirection: "column" }}
+        renderItem={({ item, index }) => {
+          return (
+            <TouchableOpacity style={{ alignItems: "center", justifyContent: "center", borderWidth: 1, borderColor: "#4c4c4c", margin: 10,borderRadius:10, }}>
+              <Image
+                source={item.image}
+                style={{ width: 80, height: 80, resizeMode: "contain" }}
+              />
+              <Text style={{ fontSize: 16, fontWeight: "700", marginTop: 10, }}>{item.type}</Text>
+            </TouchableOpacity>
+          )
+        }}
+      />
     </SafeAreaView>
   )
 }

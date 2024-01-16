6a5a2b5ee9453220707daba7f3017fcc06e6ca8b
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2024-01-16 23:02:25 +0530

    feat - started with services screen

--- src/screens/servicesScreen/index.js
+++ src/screens/servicesScreen/index.js
@@ -8,50 +8,74 @@ const services = [
   {
     type: "Hair Cut",
     time: "20 min",
-    slot: "available"
+    slot: "available",
+    description: "best hair in the world",
+    price: "Rs 150"
   },
   {
     type: "Shaving",
     time: "12 min",
-    slot: "available"
+    slot: "available",
+    description: "This hair cut will bring your confidence back",
+    price: "Rs 100"
   },
   {
     type: "Hair coloring",
     time: "50 min",
-    slot: "available"
+    slot: "booked",
+    description: "The color of your passion",
+    price: "Rs 350"
   },
   {
     type: "Hair Cut",
     time: "30 min",
-    slot: "available"
+    slot: "available",
+    description: "Style with attitude",
+    price: "Rs 200"
   },
   {
     type: "Hair Cut",
     time: "40 min",
-    slot: "available"
+    slot: "available",
+    description: "This hair cut will bring your confidence back",
+    price: "Rs 160"
   },
   {
     type: "Hair Cut",
     time: "35 min",
-    slot: "available"
+    slot: "available",
+    description: "This hair cut will bring your confidence back",
+    price: "Rs 130"
   },
-]
+];
 
 const ServicesScreen = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.mainContainer}>
       <FlatList
+        showsVerticalScrollIndicator={false}
         data={services}
-        style={{ flexDirection: "column" }}
         renderItem={({ item, index }) => {
           return (
-            <TouchableOpacity style={{ alignItems: "center", justifyContent: "center", borderWidth: 1, borderColor: "#4c4c4c", margin: 10,borderRadius:10, }}>
-              <Image
-                source={item.image}
-                style={{ width: 80, height: 80, resizeMode: "contain" }}
-              />
-              <Text style={{ fontSize: 16, fontWeight: "700", marginTop: 10, }}>{item.type}</Text>
-            </TouchableOpacity>
+            <>
+              <View style={{ flex: 1, borderWidth: 1, borderColor: "#4c4c4c", margin: 10, borderRadius: 10, padding: 15, }}>
+                <Text style={{ fontSize: 23, fontWeight: "700", marginTop: 10, }}>{item.type}</Text>
+                <Text style={{ fontSize: 13, fontWeight: "700", marginTop: 10, alignSelf: "center" }}>{item.description}</Text>
+                <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+
+                  <Text style={{ fontSize: 16, fontWeight: "700", marginTop: 10, color: "green" }}>{item.price}</Text>
+                  {item.slot === "available" ? (
+
+                    <Text style={{ fontSize: 16, fontWeight: "700", marginTop: 10, color: "green" }}>{item.slot}</Text>
+                  ) : (
+                    <Text style={{ fontSize: 16, fontWeight: "700", marginTop: 10, color: "red" }}>{item.slot}</Text>
+                  )}
+                </View>
+              </View>
+              <TouchableOpacity style={{alignSelf:"center",backgroundColor:"#0096ff"}}>
+                <Text>Book</Text>
+              </TouchableOpacity>
+            </>
           )
         }}
       />
--- src/screens/servicesScreen/style.js
+++ src/screens/servicesScreen/style.js
@@ -1,7 +1,10 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-
+    mainContainer: {
+        flex: 1,
+        backgroundColor: "#ffffff",
+    }
 });
 
 export default styles;
